ed4c559f1e33c70c50f033842d05d4247ad87816
asadhasnain <asadhasnain@gmail.com>
2018-04-14 13:26:06 +0500

    Introduced required controllers and views against them

---
+++ EventsManager/Controllers/ExpensesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EventsManagerModels;
+
+namespace EventsManager.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private DomainModels db = new DomainModels();
+
+        // GET: Expenses
+        public ActionResult Index()
+        {
+            var expenses = db.Expenses.Include(e => e.Event).Include(e => e.Member);
+            return View(expenses.ToList());
+        }
+
+        // GET: Expenses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expense expense = db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expense);
+        }
+
+        // GET: Expenses/Create
+        public ActionResult Create()
+        {
+            ViewBag.EventID = new SelectList(db.Events, "Id", "Title");
+            ViewBag.MemberID = new SelectList(db.Members, "Id", "Id");
+            return View();
+        }
+
+        // POST: Expenses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Amount,Description,MemberID,EventID,DateTime")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Expenses.Add(expense);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EventID = new SelectList(db.Events, "Id", "Title", expense.EventID);
+            ViewBag.MemberID = new SelectList(db.Members, "Id", "Id", expense.MemberID);
+            return View(expense);
+        }
+
+        // GET: Expenses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expense expense = db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EventID = new SelectList(db.Events, "Id", "Title", expense.EventID);
+            ViewBag.MemberID = new SelectList(db.Members, "Id", "Id", expense.MemberID);
+            return View(expense);
+        }
+
+        // POST: Expenses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Amount,Description,MemberID,EventID,DateTime")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(expense).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EventID = new SelectList(db.Events, "Id", "Title", expense.EventID);
+            ViewBag.MemberID = new SelectList(db.Members, "Id", "Id", expense.MemberID);
+            return View(expense);
+        }
+
+        // GET: Expenses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expense expense = db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expense);
+        }
+
+        // POST: Expenses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Expense expense = db.Expenses.Find(id);
+            db.Expenses.Remove(expense);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EventsManager/Controllers/MembersController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EventsManagerModels;
+
+namespace EventsManager.Controllers
+{
+    public class MembersController : Controller
+    {
+        private DomainModels db = new DomainModels();
+
+        // GET: Members
+        public ActionResult Index()
+        {
+            var members = db.Members.Include(m => m.Committee).Include(m => m.Employee);
+            return View(members.ToList());
+        }
+
+        // GET: Members/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        // GET: Members/Create
+        public ActionResult Create()
+        {
+            ViewBag.CommitteeID = new SelectList(db.Committees, "Id", "Title");
+            ViewBag.EmployeeID = new SelectList(db.Employees, "Id", "Name");
+            return View();
+        }
+
+        // POST: Members/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,EmployeeID,CommitteeID")] Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Members.Add(member);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CommitteeID = new SelectList(db.Committees, "Id", "Title", member.CommitteeID);
+            ViewBag.EmployeeID = new SelectList(db.Employees, "Id", "Name", member.EmployeeID);
+            return View(member);
+        }
+
+        // GET: Members/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CommitteeID = new SelectList(db.Committees, "Id", "Title", member.CommitteeID);
+            ViewBag.EmployeeID = new SelectList(db.Employees, "Id", "Name", member.EmployeeID);
+            return View(member);
+        }
+
+        // POST: Members/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,EmployeeID,CommitteeID")] Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(member).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CommitteeID = new SelectList(db.Committees, "Id", "Title", member.CommitteeID);
+            ViewBag.EmployeeID = new SelectList(db.Employees, "Id", "Name", member.EmployeeID);
+            return View(member);
+        }
+
+        // GET: Members/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        // POST: Members/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Member member = db.Members.Find(id);
+            db.Members.Remove(member);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
