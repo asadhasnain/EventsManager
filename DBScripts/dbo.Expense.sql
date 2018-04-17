d62a8c99cdcc20117f925af512edd94e45be0829
asadhasnain <asadhasnain@gmail.com>
2018-04-17 12:22:58 +0500

    Expense Table updated

--- DBScripts/dbo.Expense.sql
+++ DBScripts/dbo.Expense.sql
@@ -2,11 +2,8 @@
     [Id]          INT            IDENTITY (1, 1) NOT NULL,
     [Amount]      REAL           NOT NULL,
     [Description] NVARCHAR (250) NULL,
-    [MemberID]    INT            NOT NULL,
     [EventID]     INT            NOT NULL,
     [DateTime]    DATETIME       NOT NULL,
     CONSTRAINT [PK_Expenses] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Expense_ToEvent] FOREIGN KEY ([EventID]) REFERENCES [dbo].[Event] ([Id]),
-    CONSTRAINT [FK_Expense_ToMember] FOREIGN KEY ([MemberID]) REFERENCES [dbo].[Member] ([Id])
+    CONSTRAINT [FK_Expense_ToEvent] FOREIGN KEY ([EventID]) REFERENCES [dbo].[Event] ([Id])
 );
-
