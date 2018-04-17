72a8ac9c648113ed5ae561f617b6727fc17a4692
asadhasnain <asadhasnain@gmail.com>
2018-04-17 13:07:10 +0500

    Updating Expense table in All DBs script

--- DBScripts/All_DBs.sql
+++ DBScripts/All_DBs.sql
@@ -51,12 +51,10 @@ CREATE TABLE [dbo].[Expense] (
     [Id]          INT            IDENTITY (1, 1) NOT NULL,
     [Amount]      REAL           NOT NULL,
     [Description] NVARCHAR (250) NULL,
-    [MemberID]    INT            NOT NULL,
     [EventID]     INT            NOT NULL,
     [DateTime]    DATETIME       NOT NULL,
     CONSTRAINT [PK_Expenses] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Expense_ToEvent] FOREIGN KEY ([EventID]) REFERENCES [dbo].[Event] ([Id]),
-    CONSTRAINT [FK_Expense_ToMember] FOREIGN KEY ([MemberID]) REFERENCES [dbo].[Member] ([Id])
+    CONSTRAINT [FK_Expense_ToEvent] FOREIGN KEY ([EventID]) REFERENCES [dbo].[Event] ([Id])
 );
 
 CREATE TABLE [dbo].[Marriage] (
